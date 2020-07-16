d58d14b843999b4ae92954debebb51224fd426a3
nic <nwanavit@hatari.cc>
2020-07-16 11:56:38 +0700

    Initial Commit

---
+++ hatariIotDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hatariIotDemo
+//
+//  Created by nic wanavit on 7/16/20.
+//  Copyright © 2020 nic wanavit. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
